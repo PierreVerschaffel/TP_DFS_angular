a7d1947ea9fec55d73ba01a2fcdd1c9cb47e9127
PierreVerschaffel <pierre23021999@gmail.com>
2024-06-13 15:45:53 +0200

    first commit

---
+++ dfs_discord_backend/src/serveur/serveur.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { Salon } from 'src/salon/salon.schema';
+
+export type ServeurDocument = Serveur & Document;
+
+@Schema()
+export class Serveur {
+  @Prop({ required: true, minlength: 3, maxlength: 50 })
+  nom: string;
+
+  @Prop({ maxlength: 100 })
+  description: string;
+
+  @Prop()
+  urlLogo: string;
+
+  @Prop()
+  public: boolean;
+
+  @Prop()
+  salons: Salon[];
+}
+
+export const ServeurSchema = SchemaFactory.createForClass(Serveur);
